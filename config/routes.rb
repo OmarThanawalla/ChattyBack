416d39df3a28875026c0acc9fa85efc4a1fcabd0
Omar Thanawalla <omar.thanawalla@gmail.com>
2012-06-21 23:43:43 -0500

    added normal routing to rest

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ ChattyBackend::Application.routes.draw do
 
   resources :user, :conversation, :message, :follow, :follower, :my_conversation, :inner_conversation, :get_message, :search_user, :has_friendship
   
-  
+   match ':controller(/:action(/:id(.:format)))'
 end
