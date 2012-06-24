64eb4c0f0a9f2a0af4a2d6c79bcb6717f9dd51b0
Omar Thanawalla <omar.thanawalla@gmail.com>
2012-06-23 21:09:07 -0500

    just added some seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+ User.create(:first_name => "Omar", :last_name => "Thanawalla", :email => "omar.thanawalla@gmail.com", :hashed_password => "secretPassword")
+ User.create(:first_name => "Dwayne", :last_name => "Wade", :email => "DWade@yahoo.com", :hashed_password => "secretPassword")
